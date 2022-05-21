530e07bca1a22e17c5141a7f50f4cb6522783968
cavadsalman <cavad2013@gmail.com>
2022-05-21 13:53:17 +0400

    shared files

--- webpack.prod.js
+++ webpack.prod.js
@@ -69,9 +69,6 @@ module.exports = (env) => {
       },
       mode: 'production',
       optimization: {
-         splitChunks: {
-            chunks: 'all'
-         },
          minimizer: [
             `...`,
             new CssMinimizerPlugin(),
